a0ebb88119dc87c58cd0b0e6098defd07bf1acad
Gabibbocomrade <Tank2001@hotmail.it>
2025-10-24 12:45:39 +0200

    Corrected RunStandalone

--- MATLAB_preliminaries/RunStandalone.m
+++ MATLAB_preliminaries/RunStandalone.m
@@ -1,9 +1,9 @@
 % Script: run_standalone.m
-addpath('../AD_CTRL/');
+addpath('../CMEPDA_project_2024/AD_CTRL/');
 
-dirAD = '../AD_CTRL/AD_s3/';
-dirCTRL = '../AD_CTRL/CTRL_s3/';
-tivPath = '../AD_CTRL/covariateADCTRLsexAgeTIV.csv';
+dirAD = '../CMEPDA_project_2024/AD_CTRL/AD_s3/';
+dirCTRL = '../CMEPDA_project_2024/AD_CTRL/CTRL_s3/';
+tivPath = '../CMEPDA_project_2024/AD_CTRL/covariateADCTRLsexAgeTIV.csv';
 
 % Chiama la funzione SENZA l'handle python
 Preliminaries(dirAD, dirCTRL, tivPath);
