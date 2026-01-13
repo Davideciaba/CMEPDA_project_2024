3e347e3e99346e597e4d0f66bdb063b13c414b20
utente <davideciaba980@gmail.com>
2026-01-13 17:10:59 +0100

    Old RunStandalone deleted

--- MATLAB_preliminaries/RunStandalone.m
+++
@@ -1,9 +0,0 @@
-% Script: run_standalone.m
-addpath('../CMEPDA_project_2024/AD_CTRL/');
-
-dirAD = '../CMEPDA_project_2024/AD_CTRL/AD_s3/';
-dirCTRL = '../CMEPDA_project_2024/AD_CTRL/CTRL_s3/';
-tivPath = '../CMEPDA_project_2024/AD_CTRL/covariateADCTRLsexAgeTIV.csv';
-
-% Chiama la funzione SENZA l'handle python
-Preliminaries(dirAD, dirCTRL, tivPath);
